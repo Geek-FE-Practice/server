adeedd88957fb1b322e0a82f6c9c5c30d88300cb
luohuidong <luohuidong01@126.com>
2022-09-19 23:45:05 +0800

    refactor: 兼容 beacon 发送的数据

--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -1,17 +1,21 @@
 import type { App } from "./types";
-import bodyParser from "koa-bodyparser"
-
+import bodyParser from "koa-bodyparser";
 
 import logger from "./logger";
 import jsonError from "./jsonError";
-
 
 /**
  * 注册 middleware
  * @param app
  */
 export default function middleware(app: App): void {
-  app.use(bodyParser())
+  app.use(
+    bodyParser({
+      extendTypes: {
+        json: ["application/json", "text/plain"],
+      },
+    })
+  );
   logger(app);
   jsonError(app);
 }
