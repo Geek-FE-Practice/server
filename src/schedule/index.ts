5e5a9741cdd6ef304e09d6132b280f728d8cfbe1
luohuidong <luohuidong01@126.com>
2022-09-21 20:37:45 +0800

    feat: 定时任务每天 00:01:00 执行

--- src/schedule/index.ts
+++ src/schedule/index.ts
@@ -1,154 +1,145 @@
-// const schedule = require("node-schedule");
+const schedule = require("node-schedule");
 import { client } from "../db";
 import { spms, funnelCharts } from "../models";
 
-// const job = schedule.scheduleJob("42 * * * *", function () {
-//   console.log("The answer to life, the universe, and everything!");
-// });
-client.connect().then(async () => {
-  const current = new Date();
-  current.setHours(0, 1, 1);
-  const startTime = Math.floor(current.valueOf() / 1000);
+/** 每天 00:01:00 执行自动化任务 */
+const job = schedule.scheduleJob("1 0 * * *", function () {
+  function paddingZero(num: number) {
+    if (num > 9) {
+      return String(num);
+    } else {
+      return `0${num}`;
+    }
+  }
 
-  current.setHours(23, 59, 59);
-  const endTime = Math.floor(current.valueOf() / 1000);
+  client.connect().then(async () => {
+    const current = new Date();
+    /** 时间设置为前一天 */
+    current.setDate(current.getDate() - 1);
 
-  const year = current.getFullYear();
-  const month = current.getMonth();
-  const date = current.getDate();
+    current.setHours(0, 1, 1);
+    const startTime = Math.floor(current.valueOf() / 1000);
 
-  const formatDate = `${year}-${month}-${date}`;
+    current.setHours(23, 59, 59);
+    const endTime = Math.floor(current.valueOf() / 1000);
 
-  const project = {
-    _id: 0,
-    spm: "$_id",
-    type: "$type",
-    total: "$count",
-    date: formatDate,
-  };
+    const year = current.getFullYear();
+    const month = paddingZero(current.getMonth() + 1);
+    const date = paddingZero(current.getDate());
 
-  function getGroup(_id: Record<string, string>) {
-    return {
-      _id,
-      type: { $first: "$type" },
-      count: { $sum: 1 },
+    const formatDate = `${year}-${month}-${date}`;
+
+    const project = {
+      _id: 0,
+      spm: "$_id",
+      type: "$type",
+      total: "$count",
+      date: formatDate,
     };
-  }
 
-  interface Result {
-    spm: string;
-    type: "click" | "exposure";
-    total: number;
-    date: string;
-  }
+    function getGroup(_id: Record<string, string>) {
+      return {
+        _id,
+        type: { $first: "$type" },
+        count: { $sum: 1 },
+      };
+    }
 
-  // 聚合曝光数据
-  const result = await spms
-    .aggregate<Result>([
-      {
-        $match: {
-          time: { $gte: startTime, $lte: endTime },
-          type: "exposure",
+    interface Result {
+      spm: string;
+      type: "click" | "exposure";
+      total: number;
+      date: string;
+    }
+
+    // 聚合 a 的数据
+    const aResult = await spms
+      .aggregate<Result>([
+        {
+          $match: {
+            time: { $gte: startTime, $lte: endTime },
+            type: "click",
+          },
         },
-      },
-      {
-        $group: getGroup({
-          a: "$spm.a",
-        }),
-      },
-      { $project: project },
-    ])
-    .toArray();
-  console.log("result", result);
-
-  // 聚合 a 的数据
-  const aResult = await spms
-    .aggregate<Result>([
-      {
-        $match: {
-          time: { $gte: startTime, $lte: endTime },
-          type: "click",
+        {
+          $group: getGroup({
+            a: "$spm.a",
+          }),
         },
-      },
-      {
-        $group: getGroup({
-          a: "$spm.a",
-        }),
-      },
-      { $project: project },
-    ])
-    .toArray();
-  console.log("aResult", aResult);
+        { $project: project },
+      ])
+      .toArray();
+    console.log("aResult", aResult);
 
-  // 聚合 a.b 的数据
-  const bResult = await spms
-    .aggregate<Result>([
-      {
-        $match: {
-          time: { $gte: startTime, $lte: endTime },
-          type: "click",
+    // 聚合 a.b 的数据
+    const bResult = await spms
+      .aggregate<Result>([
+        {
+          $match: {
+            time: { $gte: startTime, $lte: endTime },
+            type: "click",
+          },
         },
-      },
-      {
-        $group: getGroup({
-          a: "$spm.a",
-          b: "$spm.b",
-        }),
-      },
-      { $project: project },
-    ])
-    .toArray();
-  console.log("bResult", bResult);
-
-  // 聚合 a.b.c 的数据
-  const cResult = await spms
-    .aggregate<Result>([
-      {
-        $match: {
-          time: { $gte: startTime, $lte: endTime },
-          type: "click",
+        {
+          $group: getGroup({
+            a: "$spm.a",
+            b: "$spm.b",
+          }),
         },
-      },
-      {
-        $group: getGroup({
-          a: "$spm.a",
-          b: "$spm.b",
-          c: "$spm.c",
-        }),
-      },
-      { $project: project },
-    ])
-    .toArray();
-  console.log("cResult", cResult);
+        { $project: project },
+      ])
+      .toArray();
+    console.log("bResult", bResult);
 
-  // 聚合 a.b.c.d 的数据
-  const dResult = await spms
-    .aggregate<Result>([
-      {
-        $match: {
-          time: { $gte: startTime, $lte: endTime },
-          type: "click",
+    // 聚合 a.b.c 的数据
+    const cResult = await spms
+      .aggregate<Result>([
+        {
+          $match: {
+            time: { $gte: startTime, $lte: endTime },
+            type: "click",
+          },
         },
-      },
-      {
-        $group: getGroup({
-          a: "$spm.a",
-          b: "$spm.b",
-          c: "$spm.c",
-          d: "$spm.d",
-        }),
-      },
-      { $project: project },
-    ])
-    .toArray();
+        {
+          $group: getGroup({
+            a: "$spm.a",
+            b: "$spm.b",
+            c: "$spm.c",
+          }),
+        },
+        { $project: project },
+      ])
+      .toArray();
+    console.log("cResult", cResult);
 
-  console.log("dResult", dResult);
+    // 聚合 a.b.c.d 的数据
+    const dResult = await spms
+      .aggregate<Result>([
+        {
+          $match: {
+            time: { $gte: startTime, $lte: endTime },
+            type: "click",
+          },
+        },
+        {
+          $group: getGroup({
+            a: "$spm.a",
+            b: "$spm.b",
+            c: "$spm.c",
+            d: "$spm.d",
+          }),
+        },
+        { $project: project },
+      ])
+      .toArray();
 
-  await funnelCharts.insertMany(result);
-  await funnelCharts.insertMany(aResult);
-  await funnelCharts.insertMany(bResult);
-  await funnelCharts.insertMany(cResult);
-  await funnelCharts.insertMany(dResult);
+    console.log("dResult", dResult);
 
-  await client.close();
+    await funnelCharts.insertMany(aResult);
+    await funnelCharts.insertMany(bResult);
+    await funnelCharts.insertMany(cResult);
+    await funnelCharts.insertMany(dResult);
+
+    await client.close();
+  });
 });
